d84d64eca52946e9758aa02bf7ff03ba953562ab
zlargon <zlargon1988@gmail.com>
2018-03-25 11:00:13 -0400

    [server] implement login API

--- public/sdk.js
+++ public/sdk.js
@@ -22,3 +22,27 @@ function register (username, password) {
   .then(console.log)
   .catch(console.error);
 }
+
+// Login
+function login (username, password) {
+  fetch(`${host}/login`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username: username,
+      password: sha1(password)
+    }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  .then(res => res.json())
+  .then(res => {
+    if (res.status !== 200) {
+      throw new Error(res.message);
+    }
+    return res.data;
+  })
+  .then(console.log)
+  .catch(console.error);
+}
+
--- routes/index.js
+++ routes/index.js
@@ -46,4 +46,48 @@ router.post('/register', function (req, res) {
   });
 });
 
+// login
+router.post('/login', function (req, res) {
+
+  const username = req.body.username;
+  const password = sha1(req.body.password);
+  const user = DB.userList[username];
+
+  // check userList
+  if (typeof user === 'undefined') {
+    res.status(400);
+    res.send({
+      status: 400,
+      message: 'username does not exist',
+      data: {
+        username: username
+      }
+    });
+    return;
+  }
+
+  // check password
+  if (user.password !== password) {
+    res.status(400);
+    res.send({
+      status: 400,
+      message: 'password is incorrect',
+      data: {
+        username: username
+      }
+    });
+    return;
+  }
+
+  res.status(200);
+  res.send({
+    status: 200,
+    message: 'login success',
+    data: {
+      username: username,
+      token: user.token
+    }
+  });
+});
+
 module.exports = router;

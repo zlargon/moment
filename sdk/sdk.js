64435cf54ad457126d20b2abbc104fc2c2db5aa6
zlargon <zlargon1988@gmail.com>
2018-03-26 14:13:01 -0400

    [sdk] create sdk.js

---
+++ sdk/sdk.js
@@ -0,0 +1,24 @@
+const co = require('co');
+const fetch = require('node-fetch');
+const sha1 = require('js-sha1');
+const host = 'http://localhost:3000';
+
+// Register
+const sdk = {
+  register: function (username, password, email) {
+    return fetch(`${host}/user`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: sha1(password),
+        email: email
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json());
+  }
+}
+
+module.exports = sdk;

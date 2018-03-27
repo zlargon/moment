fd34696185e4f876b370e72a9c46d4380eae53ce
zlargon <zlargon1988@gmail.com>
2018-03-26 20:15:30 -0400

    [refactor] remove old code

--- routes/index.js
+++ routes/index.js
@@ -11,32 +11,7 @@ router.get('/', function(req, res, next) {
   res.sendfile('./public/index.html');
 });
 
-// Database
-const database = {
-  user_pw_tk: {},   // {
-                    //   username: {
-                    //     password: 'xxx',
-                    //     token: 'xxx'
-                    //   }
-                    // }
-
-  user_profile: {}, // {
-                    //   user_token: {
-                    //     username: 'xxx',
-                    //     article_id: []
-                    //   }
-                    // }
-
-  article: {}       // {
-                    //   article_id: {
-                    //     timestamp: '',
-                    //     author: '',
-                    //     content: ''
-                    //   }
-                    // }
-};
-
-// user login
+// 1. user login
 router.get('/auth', function (req, res) {
 
   User.findOne({
@@ -74,109 +49,6 @@ router.get('/auth', function (req, res) {
         data: {}
       });
     });
-});
-
-// post article
-router.post('/article', function (req, res) {
-  const username = req.body.username;
-  const user_token = req.body.token;
-  const article_content = req.body.article;
-
-  // verify the user and token
-  const user = database.user_profile[user_token];
-  if (typeof user === 'undefined' || user.username !== username) {
-    res.status(400);
-    res.send({
-      status: 400,
-      message: 'token is unauthorized',
-      data: {
-        username: username
-      }
-    });
-    return;
-  }
-
-  const article_id = shortid.generate();
-  database.article[article_id] = {
-    time: new Date(),
-    author: username,
-    content: article_content
-  };
-  database.user_profile[user_token].article_id.push(article_id);
-
-  res.status(200);
-  res.send({
-    status: 200,
-    message: 'post success',
-    data: {
-      username: username,
-      article_id: article_id
-    }
-  });
-});
-
-// get add article
-router.get('/article', function (req, res) {
-  const article_list = Object.values(database.article);
-
-  res.status(200);
-  res.send({
-    status: 200,
-    message: 'get articles success',
-    data: {
-      articles: article_list
-    }
-  });
-});
-
-// get article
-router.get('/article/:username_or_articleid', function (req, res) {
-
-  const id = req.params.username_or_articleid;
-
-  // article is found
-  if (typeof database.article[id] !== 'undefined') {
-    res.status(200);
-    res.send({
-      status: 200,
-      message: 'get article success',
-      data: {
-        article: database.article[id]
-      }
-    });
-    return;
-  }
-
-  // username is found
-  const user = database.user_pw_tk[id];
-  if (typeof user !== 'undefined') {
-    const article = database.user_profile[user.token].article_id.map(o => database.article[o]);
-    res.status(200);
-    res.send({
-      status: 200,
-      message: `get user's articles success`,
-      data: {
-        article: article
-      }
-    });
-    return;
-  }
-
-  res.status(400);
-  res.send({
-    status: 400,
-    message: 'username or article_id is not found',
-    data: {}
-  });
-});
-
-router.get('/debug', function (req, res) {
-  User.find()
-    .exec()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(console.error);
 });
 
 module.exports = router;

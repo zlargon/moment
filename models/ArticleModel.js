7df8966332bed19e0b16d0ed9971caa6296c55de
zlargon <zlargon1988@gmail.com>
2019-12-22 20:53:44 -0800

    [mongodb] fix issue: MongoError: Unknown modifier: $pushAl

--- models/ArticleModel.js
+++ models/ArticleModel.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
 const articleSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  timestamp: {
-    type: Date,
-    required: true
+    _id: mongoose.Schema.Types.ObjectId,
+    timestamp: {
+      type: Date,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    body: {
+      type: String,
+      required: true
+    },
+    comment: Array,
+    like: Array
   },
-  author: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  body: {
-    type: String,
-    required: true
-  },
-  comment: Array,
-  like: Array
-});
+  { // https://github.com/StephenGrider/Lyrical-GraphQL/issues/17#issuecomment-413799895
+    usePushEach : true
+  }
+);
 
 module.exports = mongoose.model('Article', articleSchema);
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,25 +1,29 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    trim: true
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: String,
+    token: {
+      type: String,
+      required: true
+    },
+    article: Array
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: String,
-  token: {
-    type: String,
-    required: true
-  },
-  article: Array
-});
+  { // https://github.com/StephenGrider/Lyrical-GraphQL/issues/17#issuecomment-413799895
+    usePushEach : true
+  }
+);
 
 module.exports = mongoose.model('User', userSchema);
